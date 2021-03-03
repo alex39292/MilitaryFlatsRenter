fa3b4dc4ed79b2ef1487607779eaf4cf38816d44
Aliaksandr Danilin <aliaksandr_danilin1@epam.com>
2021-03-04 00:46:24 +0300

    implement yargs

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 'use strict';
 
+const yargs = require('yargs').argv;
 const { log4js } = require('./utils/log4js');
 const logger = log4js.getLogger('application');
 const { startLoop, findHome } = require('./models/homes');
 const { getUsers, getUserById, createUser, changeState, insertCity } = require('./models/users');
 const { Telegraf } = require('telegraf');
-const bot = new Telegraf();
+const bot = new Telegraf(yargs.token);
 
 startLoop();
