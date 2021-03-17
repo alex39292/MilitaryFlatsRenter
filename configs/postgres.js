1ca5f174805d98611b83dad90a6bad74c379479e
Aliaksandr Danilin <aliaksandr_danilin1@epam.com>
2021-03-17 18:15:57 +0300

    add postgres options

--- configs/postgres.js
+++ configs/postgres.js
@@ -1,4 +1,9 @@
 module.exports = {
     //DATABASE_URL: process.env.DATABASE_URL
-    DATABASE_URL:'postgres://qvexaqyzaljxia:9463ad0a87b0096364f634@ec2-176-34-222-188.eu-west-1.compute.amazonaws.com:5432/d3640i4s5u3hqi'
+    DATABASE_URL:'postgres://qvexaqyzaljxia:9463ad0a87b0096364f634@ec2-176-34-222-188.eu-west-1.compute.amazonaws.com:5432/d3640i4s5u3hqi',
+    user: 'qvexaqyzaljxia',
+    host: 'ec2-176-34-222-188.eu-west-1.compute.amazonaws.com',
+    database: 'd3640i4s5u3hqi',
+    password: '9463ad0a87b0096364f634199b5765905e3b4a738d7d67f93f884be21657a2c8',
+    port: 5432,
 };
