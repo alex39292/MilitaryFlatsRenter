b36f0f3c1d2d7ff833bd8a486e3b9263185f1ff4
Aliaksandr Danilin <aliaksandr_danilin1@epam.com>
2021-03-17 20:19:07 +0300

    change domain

--- configs/bot.js
+++ configs/bot.js
@@ -1,8 +1,8 @@
 module.exports = {
     webhook: {
         //domain: process.env.webhook,
-        sdomain:'https://militaryflats.herokuapp.com/',
-        domain: 'https://fd5591fcce09.ngrok.io',
+        domain:'https://militaryflats.herokuapp.com/',
+        sdomain: 'https://fd5591fcce09.ngrok.io',
         port: process.env.port || 5000
     }
 }
