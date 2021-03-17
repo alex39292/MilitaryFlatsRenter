cf081c15f6333f054557a42eb3554e242f033c1a
Aliaksandr Danilin <aliaksandr_danilin1@epam.com>
2021-03-17 18:59:46 +0300

    sss

--- app.js
+++ app.js
@@ -108,6 +108,9 @@ app.use(bot.webhookCallback('/'));
 require('https').createServer().listen(process.env.PORT || 5000).on('request', () => {
     logger.info('Listening app on port 5000');
 });
+app.listen(5000, () => {
+    logger.info('Listening app on port 5000');
+})
 
 async function broadcast(id) {
     const city = await getCityById(id);
