cf852940e592ce05f66043513e29e5d3b5a674f3
Aliaksandr Danilin <aliaksandr_danilin1@epam.com>
2021-03-17 18:55:54 +0300

    d

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ app.post('/message', async (req, res) => {
 });
 
 app.use(bot.webhookCallback('/'));
-app.listen(configs.webhook.port || 5000, '0.0.0.0', () => {
+require('https').createServer().listen(process.env.PORT || 5000).on('request', () => {
     logger.info('Listening app on port 5000');
 });
 
