8b8b04aa27530f087417be41292e1d16c58618d4
Aliaksandr Danilin <aliaksandr_danilin1@epam.com>
2021-03-17 20:12:49 +0300

    cdcdc

--- utils/clean_up.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const fs = require('fs-extra');
-const path = require('path');
-
-const logsLocation = path.resolve(__dirname, '../logs');
-fs.emptydirSync(logsLocation);
--- utils/log4js.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-const log4js_config = require('../configs/log4js');
-
-module.exports.log4js = require('log4js').configure(log4js_config);
