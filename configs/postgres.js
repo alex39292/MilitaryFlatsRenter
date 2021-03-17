79af1a0a5061b133e74e7f143079cf87fa126424
Aliaksandr Danilin <aliaksandr_danilin1@epam.com>
2021-03-17 17:54:07 +0300

    fix strings

--- configs/postgres.js
+++ configs/postgres.js
@@ -1,4 +1,4 @@
 module.exports = {
     //DATABASE_URL: process.env.DATABASE_URL
-DATABASE_URL:postgres://qvexaqyzaljxia:9463ad0a87b0096364f634@ec2-176-34-222-188.eu-west-1.compute.amazonaws.com:5432/d3640i4s5u3hqi
+    DATABASE_URL:'postgres://qvexaqyzaljxia:9463ad0a87b0096364f634@ec2-176-34-222-188.eu-west-1.compute.amazonaws.com:5432/d3640i4s5u3hqi'
 };
