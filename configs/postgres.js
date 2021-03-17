1cc4b15874d01dfd980478ebea1c80ce83879891
Aliaksandr Danilin <aliaksandr_danilin1@epam.com>
2021-03-17 18:25:52 +0300

    add configs so ssl

--- configs/postgres.js
+++ configs/postgres.js
@@ -6,5 +6,7 @@ module.exports = {
     database: 'd3640i4s5u3hqi',
     password: '9463ad0a87b0096364f634199b5765905e3b4a738d7d67f93f884be21657a2c8',
     port: 5432,
-    ssl: true
+    ssl: {
+        rejectUnauthorized: false
+    }
 };
