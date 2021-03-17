96b65c5fba5cf25f93b6573e9cfacbef4e6d97f0
alex39292 <50744099+alex39292@users.noreply.github.com>
2021-03-17 11:20:30 +0300

    change

--- configs/bot.js
+++ configs/bot.js
@@ -1,6 +1,7 @@
 module.exports = {
     webhook: {
-        domain: process.env.webhook,
+        //domain: process.env.webhook,
+domain:1691534515:AAFJR6hh6POubgyzPCesttZ-b9CXpDfYEog,
         port: process.env.port || 5000
     }
 }
