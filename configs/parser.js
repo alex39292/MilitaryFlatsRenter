92f552eb79144cb2aabc091b0eff1c403ad356f8
alex39292 <50744099+alex39292@users.noreply.github.com>
2021-03-25 15:05:13 +0300

    Update selector

--- configs/parser.js
+++ configs/parser.js
@@ -2,7 +2,7 @@ module.exports = {
     url: process.env.url,
     selectors: {
         id: 'div:nth-child(7) > table > tbody > tr:nth-child(n + 3) > td:nth-child(1) > p',
-        address: 'div:nth-child(7) > table > tbody > tr:nth-child(n + 3) > td:nth-child(2) > p',
+        address: 'div:nth-child(7) > table > tbody > tr:nth-child(n + 3) > td:nth-child(2)',
         floor: 'div:nth-child(7) > table > tbody > tr:nth-child(n + 3) > td:nth-child(3) > p',
         flats: 'div:nth-child(7) > table > tbody > tr:nth-child(n + 3) > td:nth-child(4) > p',
         area: 'tr:nth-child(n + 3) > td:nth-child(5) > p',
