aaa838cd439b2de783664e0a4c70d4f589e11d0c
Aliaksandr Danilin <aliaksandr_danilin1@epam.com>
2021-04-10 20:36:39 +0300

    change selectors

--- configs/parser.js
+++ configs/parser.js
@@ -1,11 +1,11 @@
 module.exports = {
     url: process.env.url,
     selectors: {
-        address: 'div:nth-child(7) > table > tbody > tr:nth-child(n + 3) > td:nth-child(2)',
-        floor: 'div:nth-child(7) > table > tbody > tr:nth-child(n + 3) > td:nth-child(3) > p',
-        flats: 'div:nth-child(7) > table > tbody > tr:nth-child(n + 3) > td:nth-child(4) > p',
-        area: 'tr:nth-child(n + 3) > td:nth-child(5) > p',
-        deadLine: 'tr:nth-child(n + 3) > td:nth-child(6) > p',
-        other: 'tr:nth-child(n + 3) > td:nth-child(7) > p'
+        address: 'table:nth-child(7) > tbody > tr:nth-child(n + 3) > td:nth-child(2)',
+        floor: 'table:nth-child(7) > tbody > tr:nth-child(n + 3) > td:nth-child(3) > p',
+        flats: 'table:nth-child(7) > tbody > tr:nth-child(n + 3) > td:nth-child(4) > p',
+        area: 'table:nth-child(7) > tbody > tr:nth-child(n + 3) > td:nth-child(5) > p',
+        deadLine: 'table:nth-child(7) > tbody > tr:nth-child(n + 3) > td:nth-child(6) > p',
+        other: 'table:nth-child(7) > tbody > tr:nth-child(n + 3) > td:nth-child(7)'
     }
 }
