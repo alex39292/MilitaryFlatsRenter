763cd132406a9aa95169f3b9ec16034e1d65eb6b
Aliaksandr Danilin <aliaksandr_danilin1@epam.com>
2021-04-12 18:52:40 +0300

    change url

--- configs/postgres.js
+++ configs/postgres.js
@@ -1,9 +1,5 @@
 module.exports = {
-    user: process.env.user,
-    host: process.env.host,
-    database: process.env.database,
-    password: process.env.password,
-    port: 5432,
+    url: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
     }
