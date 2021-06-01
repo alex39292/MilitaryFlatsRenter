57efbc6beb24f2babb0239e8e1fb3149bd0813db
Aliaksandr Danilin <aliaksandr_danilin1@epam.com>
2021-06-01 20:25:31 +0300

    rename files

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ app.use(express.static(__dirname + '/pages'));
 app.use(express.urlencoded({extended: false}));
 
 app.get('/', async (req, res) => {
-    res.render('login');
+    res.render('index');
 });
 
 app.post('/', async (req, res) => {
