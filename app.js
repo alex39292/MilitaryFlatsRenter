621ff7ad125611c485df1f651623c10cb6fccd57
alex39292 <50744099+alex39292@users.noreply.github.com>
2021-08-03 01:00:24 +0300

    set webhook

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ bot.action('Unsubscribe', async ctx => {
     await ctx.reply('Вы отписались');
 });
 
-bot.telegram.setWebhook(configs.webhook.domain);
+bot.telegram.setWebhook('https://militaryflats.herokuapp.com/');
 
 app.engine('html', es6Renderer);
 app.set('views', './pages');
