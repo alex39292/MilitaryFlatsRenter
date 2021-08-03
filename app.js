69b23c1d15988f44faf9c613999b9b94464fd721
alex39292 <50744099+alex39292@users.noreply.github.com>
2021-08-03 13:04:46 +0300

    add )

--- app.js
+++ app.js
@@ -58,12 +58,12 @@ bot.on('message', async ctx => {
     else {
     const messages = [message.substring(0,4096),message(4097,message.length)];
     messages.forEach(message => {
-      ctx.reply(message));
+      ctx.reply(message);
       ctx.reply('Подписаться на обновление?',
         Markup.inlineKeyboard([
             Markup.button.callback('🔔Подписаться', 'Subscribe')
         ]));
-      }
+      });
     }
 });
 
