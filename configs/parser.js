2614318416fa55c01721e6e10b7674bb957c67ff
alex39292 <50744099+alex39292@users.noreply.github.com>
2022-01-15 14:55:31 +0300

    Fix error

--- configs/parser.js
+++ configs/parser.js
@@ -1,11 +1,11 @@
 module.exports = {
     url: process.env.url,
     selectors: {
-        address: 'table:nth-child(1) > tbody > tr:nth-child(n + 3) > td:nth-child(2)',
-        floor: 'table:nth-child(1) > tbody > tr:nth-child(n + 3) > td:nth-child(3) > p',
-        flats: 'table:nth-child(1) > tbody > tr:nth-child(n + 3) > td:nth-child(4) > p',
-        area: 'table:nth-child(1) > tbody > tr:nth-child(n + 3) > td:nth-child(5) > p',
-        deadLine: 'table:nth-child(1) > tbody > tr:nth-child(n + 3) > td:nth-child(6) > p',
-        other: 'table:nth-child(1) > tbody > tr:nth-child(n + 3) > td:nth-child(7)'
+        address: 'div.table-responsive:nth-child(7) > table > tbody > tr:nth-child(n + 3) > td:nth-child(2)',
+        floor: 'div.table-responsive:nth-child(7) > table > tbody > tr:nth-child(n + 3) > td:nth-child(3) > p',
+        flats: 'div.table-responsive:nth-child(7) > table > tbody > tr:nth-child(n + 3) > td:nth-child(4) > p',
+        area: 'div.table-responsive:nth-child(7) > table > tbody > tr:nth-child(n + 3) > td:nth-child(5) > p',
+        deadLine: 'div.table-responsive:nth-child(7) > table > tbody > tr:nth-child(n + 3) > td:nth-child(6) > p',
+        other: 'div.table-responsive:nth-child(7) > table > tbody > tr:nth-child(n + 3) > td:nth-child(7)'
     }
 }
