598ba4233825ef41fe93cbf71240fc9dfae90ed8
Aliaksandr Danilin <alex39292@mail.ru>
2022-09-24 18:12:28 +0300

    fix bug with parser

--- services/parser.js
+++ services/parser.js
@@ -7,41 +7,44 @@ const parser = require('../configs/parser');
 module.exports.getData = async () => {
     const homes = [];
     const response = await getDOM();
-    const $ = cheerio.load(response.data);
-    const fromResponse = {
-        'address': getElementsBy(parser.selectors.address),
-        'floor': getElementsBy(parser.selectors.floor),
-        'flats': getElementsBy(parser.selectors.flats),
-        'area': getElementsBy(parser.selectors.area),
-        'deadLine': getElementsBy(parser.selectors.deadLine),
-        'notes': getElementsBy(parser.selectors.other),
-    };
-    console.log(`Got ${fromResponse.address.length} homes from response`);
-    
-    let id = 1; 
-    while (fromResponse.address.length !== 0) {
-        homes.push({
-            'id': id++,
-            'address': fromResponse.address.shift(),
-            'floor': fromResponse.floor.shift(),
-            'flats': fromResponse.flats.shift(),
-            'area': fromResponse.area.shift(),
-            'deadLine': fromResponse.deadLine.shift(),
-            'notes': fromResponse.notes.shift(),
-        });
-    }
+    if (response !== null) {
+        const $ = cheerio.load(response.data);
+        const fromResponse = {
+            'address': getElementsBy(parser.selectors.address),
+            'floor': getElementsBy(parser.selectors.floor),
+            'flats': getElementsBy(parser.selectors.flats),
+            'area': getElementsBy(parser.selectors.area),
+            'deadLine': getElementsBy(parser.selectors.deadLine),
+            'notes': getElementsBy(parser.selectors.other),
+        };
+        console.log(`Got ${fromResponse.address.length} homes from response`);
+        
+        let id = 1; 
+        while (fromResponse.address.length !== 0) {
+            homes.push({
+                'id': id++,
+                'address': fromResponse.address.shift(),
+                'floor': fromResponse.floor.shift(),
+                'flats': fromResponse.flats.shift(),
+                'area': fromResponse.area.shift(),
+                'deadLine': fromResponse.deadLine.shift(),
+                'notes': fromResponse.notes.shift(),
+            });
+        }
 
-    function getElementsBy(selector) {
-        return $(selector).toArray().map(elem => $(elem).text().trim());
+        function getElementsBy(selector) {
+            return $(selector).toArray().map(elem => $(elem).text().trim());
+        }
     }
 
     return homes;
 }
 
-async function getDOM() {
+const getDOM = () => {
     try {
-        return await axios.get(parser.url);
+        return axios('https://www.mil.by/ru/housing/commerc/');
     } catch (error) {
         console.log(error);
+        return null;
     }
 }
